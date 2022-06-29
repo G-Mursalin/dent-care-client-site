8fea79bb549b3b5d146c924133430830e49174fc
Golam Mursalin <golammursalin309@gmail.com>
2022-06-29 11:37:24 +0600

    dashboard table added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,19 +6,15 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
   return (
     <div className="drawer drawer-mobile">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        <h2 className="text-5xl text-purple-500">Dashboard</h2>
+      <input id="side-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content py-3">
+        <h2 className="text-3xl text-primary font-bold">
+          Welcome to your Dashboard
+        </h2>
         <Outlet />
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
       </div>
       <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <label htmlFor="side-drawer" className="drawer-overlay"></label>
         <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
           <li>
             <Link to="/dashboard">My Appointment</Link>
--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,7 +1,54 @@
+// React
 import React from "react";
-
+// React Query
+import { useQuery } from "react-query";
+// React Firebase Hook
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+// Components
+import Loading from "../Shared/Loading";
 const MyAppointments = () => {
-  return <div>MyAppointments</div>;
+  const [user] = useAuthState(auth);
+
+  const { data: appointments, isLoading } = useQuery("userAppointments", () =>
+    fetch(`http://localhost:5000/booking?email=${user?.email}`).then((res) =>
+      res.json()
+    )
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <h2 className="my-3">My Total Appointments: {appointments.length}</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((a, i) => (
+              <tr>
+                <th>{i + 1}</th>
+                <td>{a.patientName}</td>
+                <td>{a.date}</td>
+                <td>{a.slot}</td>
+                <td>{a.treatmentName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
